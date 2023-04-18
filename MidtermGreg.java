6c040a5cf6d43ca8c43546803c24a605bb094602
Usharov24 <130595341+Usharov24@users.noreply.github.com>
2023-04-18 13:04:58 -0400

    I have figured the out
    Once again I have learnt how to navigate this software

--- MidtermGreg.java
+++ MidtermGreg.java
@@ -4,16 +4,12 @@ import arc.*;
 public class MidtermGreg{ 
 public static void main(String []args){
     Console con = new Console();
+    con.println("Gaming time");
+    con.println("oh yeah les go");
+    con.println("please work github");
   
 
 
 
 }
-
-
-
-  
- 
- 
-
 }
