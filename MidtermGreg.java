28a9dd2e7cbfbff56b50e10a27d376f9980e7486
Usharov24 <130595341+Usharov24@users.noreply.github.com>
2023-04-18 13:05:33 -0400

    oh yeah les go
    everything is working

--- MidtermGreg.java
+++ MidtermGreg.java
@@ -4,12 +4,9 @@ import arc.*;
 public class MidtermGreg{ 
 public static void main(String []args){
     Console con = new Console();
-    con.println("Gaming time");
-    con.println("oh yeah les go");
-    con.println("please work github");
   
-
-
+con.println("gaming time");
+con.println("test");
 
 }
 }
